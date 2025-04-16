d1419be81df1d4eb4bbc5f8955ecfd9517bf8398
jysh1214 <jyxemperor@gmail.com>
2025-04-16 18:13:19 +0800

    Add single GPU flow

--- BEVFormer/tools/dist_test.sh
+++ BEVFormer/tools/dist_test.sh
@@ -6,5 +6,5 @@ GPUS=$3
 PORT=${PORT:-29503}
 
 PYTHONPATH="$(dirname $0)/..":$PYTHONPATH \
-python -m torch.distributed.launch --nproc_per_node=$GPUS --master_port=$PORT \
-    $(dirname "$0")/test.py $CONFIG $CHECKPOINT --launcher pytorch ${@:4} --eval bbox
+python3.9 -m torch.distributed.launch --nproc_per_node=$GPUS --master_port=$PORT \
+    $(dirname "$0")/test.py $CONFIG $CHECKPOINT --launcher none ${@:4} --eval bbox
